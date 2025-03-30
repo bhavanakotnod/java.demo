619699fab0e62d4eaec44f6e5a378215759638df
bhavanakotnod <bhavanakotnod@gmail.com>
2025-03-30 16:10:36 +0530

    Add files via upload

---
+++ AlphabetValue.java
@@ -0,0 +1,16 @@
+package com.Methods;
+
+public class AlphabetValue {
+	public static void main(String[] args) {
+		asciiValue(66);
+		alpha('B');
+	}
+	public static void asciiValue(int num) {
+		System.out.println("ASCII value of given number is =  "+(char)num);
+	}
+	public static void alpha(char name) {
+		System.out.println("ASCII number of given character is =  "+(int)name);
+	}
+	
+
+}
