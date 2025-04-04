bbc83a2a9e7c4d43ff08017c08fe0aa1115f15f2
bhavanakotnod <bhavanakotnod@gmail.com>
2025-04-04 18:03:57 +0530

    Add files via upload

---
+++ StaticNonStatic.java
@@ -0,0 +1,16 @@
+
+public class StaticNonStatic {
+	// Normal block , non-static block , non-instance block
+
+	{
+		System.out.println("this is non static method");
+		// this is non static method without any method creation
+	}
+	
+	static {
+		System.out.println("This is static mthod");
+	}
+	
+	
+	
+}
