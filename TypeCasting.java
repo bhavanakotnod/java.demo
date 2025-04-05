ee270af77bb4701368aa13d778bc3bb11f08ee61
bhavanakotnod <bhavanakotnod@gmail.com>
2025-04-05 22:03:56 +0530

    Add files via upload
    type casting problem .  two type of type casting  1)  Explicit  2) implicit

---
+++ TypeCasting.java
@@ -0,0 +1,20 @@
+
+public class TypeCasting {
+	public static void main(String args[])
+	{
+		
+		/*there are two types of Type csting
+		 
+		  1    Implicit
+		  2    Explicit = its forcefully convert the one data type to another data type here data loss is occour
+		 
+		  
+		 */
+		int a = 300;
+		float b =a;           // 1) implicit = directly change data from one data type to another datatype
+		System.out.println(b);
+		
+		 
+	}
+
+}
